0e05961f35942408993a80a5d9f122ca1748eb3e
ValentinoFala <valentinofala@gmail.com>
2025-10-24 11:13:20 -0300

    Update Cuadrado.java
    Implementacion tp4

--- src/Figuras/Cuadrado.java
+++ src/Figuras/Cuadrado.java
@@ -1,25 +1,58 @@
 package Figuras;
 
+/**
+ * Representa una figura geométrica del tipo cuadrado.
+ * Hereda de la clase {@code Figura} e implementa el cálculo del área
+ * a partir de la longitud de su lado.
+ * <p>
+ * Esta clase puede extenderse para incluir el cálculo del perímetro u otras
+ * operaciones específicas.
+ * 
+ * @author Valentino Falabella
+ * @version 1.0
+ */
 public class Cuadrado extends Figura {
-	
-	double lado;
+    
+    /** Longitud del lado del cuadrado. */
+    private double lado;
 
-	public Cuadrado(String nombre, double lado) {
-		super(nombre);
-		this.lado = lado;
-	}
+    /**
+     * Crea una nueva instancia de {@code Cuadrado}.
+     *
+     * @param nombre el nombre de la figura.
+     * @param lado la longitud del lado del cuadrado.
+     */
+    public Cuadrado(String nombre, double lado) {
+        super(nombre);
+        this.lado = lado;
+    }
 
-	public double getLado() {
-		return lado;
-	}
+    /**
+     * Devuelve la longitud del lado del cuadrado.
+     *
+     * @return el valor del lado.
+     */
+    public double getLado() {
+        return lado;
+    }
 
-	public void setLado(double lado) {
-		this.lado = lado;
-	}
-	
-	public double calcularArea() {
-		return this.lado * this.lado;
-	}
+    /**
+     * Establece una nueva longitud para el lado del cuadrado.
+     *
+     * @param lado el nuevo valor del lado.
+     */
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
 
-	
+    /**
+     * Calcula el área del cuadrado utilizando la fórmula:
+     * <pre>Área = lado × lado</pre>
+     *
+     * @return el área del cuadrado.
+     */
+    @Override
+    public double calcularArea() {
+        return this.lado * this.lado;
+    }
 }
