41878407ed57d341beee500179ef149ff7bbbf2e
ValentinoFala <valentinofala@gmail.com>
2025-10-24 12:22:31 -0300

    Update RectanguloTest.java
    implementacion tp4

--- src/test/RectanguloTest.java
+++ src/test/RectanguloTest.java
@@ -6,13 +6,41 @@ import Figuras.Rectangulo;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Disabled;
+/**
+ * Clase de prueba unitaria para la clase {@link Figuras.Rectangulo}.
+ * <p>
+ * Evalúa el comportamiento de los métodos {@code getPerimetro()} y {@code getArea()} 
+ * verificando los resultados esperados en función de las dimensiones establecidas.
+ * </p>
+ * 
+ * @author Valentino Falabella
+ * @version 1.0
+ */
 public class RectanguloTest {
+	/**
+     * Prueba que verifica el cálculo correcto del perímetro del rectángulo.
+     * <p>
+     * Caso de prueba positivo: se espera que el método {@code getPerimetro()} 
+     * devuelva el valor correcto (8.2) según la base y altura proporcionadas.
+     * </p>
+     */
 	@Test
 	public void perimetroTest() {
 		Rectangulo r1 = new Rectangulo(1.2, 3.2, "asd");
 		
 		assertEquals(8.2, r1.getPerimetro());
 	}
+	/**
+     * Prueba que verifica el cálculo del área del rectángulo.
+     * <p>
+     * Este test está deshabilitado temporalmente mediante {@link Disabled}, 
+     * por lo que no se ejecutará durante las pruebas automatizadas.
+     * </p>
+     * <p>
+     * Caso de prueba positivo: se espera que el método {@code getArea()} 
+     * devuelva el valor 8.2.
+     * </p>
+     */
 	@Test
 	@Disabled
 	public void areaTest() {
